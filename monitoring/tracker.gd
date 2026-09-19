e5a034d21d456d22846c34094388ce1aaf444596
Dhruv Ravinuthala <dhruv.ravinuthala@gmail.com>
2026-09-19 03:46:20 -0500

    Implemented better pathfinding and other fun stuff
    So by adding a bunch of static bodies and then adding them to my navigation mesh, the pathfinder can now optimize itself to go around walls! I also added a sensor for when the enemy makes contact with the player character, and color coded everything so it didn't all blend in.

--- monitoring/tracker.gd
+++ monitoring/tracker.gd
@@ -2,18 +2,22 @@ extends CharacterBody3D
 
 var player = null
 
-const speed = 2
+const speed = 2.5
 @export var playerPath: NodePath
 @onready var navAgent = $NavigationAgent3D
 
 func _ready() -> void:
 	player = get_node(playerPath)
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	velocity = Vector3.ZERO
-	
 	navAgent.set_target_position(player.global_position)
 	var nextNavPoint = navAgent.get_next_path_position()
 	velocity = (nextNavPoint - global_position).normalized() * speed
 
 	move_and_slide()
+
+
+func _on_area_3d_body_entered(body) -> void:
+	if body.is_in_group("caramel"):
+		print("touch")
