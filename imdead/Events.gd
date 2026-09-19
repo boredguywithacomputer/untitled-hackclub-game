a7da2fc77420e0fdcb62da807c63f13eedd6b6a0
chickenfried42 <thebigoof200005@gmail.com>
2026-09-19 00:21:37 -0700

    Created a modular proximity interaction system
    When a player goes near certain objects, it prompts them to interact with it. If the E key is pressed, the thingy fires an 'interaction' signal, which consists of an object argument (the object being interacted with) which is a Node type, and a triggeredevent arg, which is a String type.
    
    I also handled overlapping prompts.

---
+++ imdead/Events.gd
@@ -0,0 +1,4 @@
+extends Node
+signal prompt(promptname: String, object: Node, triggerevent: String)
+signal unprompt(object: Node)
+signal interaction(object: Node, triggerevent: String)
---
+++ imdead/loot_llama.gd
@@ -0,0 +1,20 @@
+extends RigidBody3D
+
+@export var promptName: String 
+@export var triggeredEvent: String
+
+func _ready() -> void:
+	$Hitbox.body_entered.connect(_proximity_trigger)
+	$Hitbox.body_exited.connect(_deproximity_trigger)
+
+func _proximity_trigger(body: Node3D) -> void:
+	if !body.name == "Player": return
+	print("player within range")
+	Events.prompt.emit(promptName, self, triggeredEvent)
+	
+func _deproximity_trigger(body: Node3D) -> void:
+	if !body.name == "Player": return
+	Events.unprompt.emit(self)
+
+func _process(delta: float) -> void:
+	pass
