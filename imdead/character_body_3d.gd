1c20eacfadab48eaa7a5e781bf2a216d340d14c7
chickenfried42 <thebigoof200005@gmail.com>
2026-09-20 00:05:11 -0700

    Made some minor tweaks
    added models for some things, fixed a few bugs, etc.

--- imdead/character_body_3d.gd
+++ imdead/character_body_3d.gd
@@ -1,14 +1,18 @@
 extends CharacterBody3D
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+var SPEED = 5.0
+var JUMP_VELOCITY = 5.0
+
+func _ready() -> void:
+	Events.slow.connect(_change_speed)
+
+func _change_speed(speed):
+	SPEED += speed
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
-	# Handle jump.
+		
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
